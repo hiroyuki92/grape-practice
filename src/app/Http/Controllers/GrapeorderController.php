6421c7a7e48a16c9e57b8d432442e7142e6582b8
yukiko <yukiko@magnolia.co.jp>
2024-10-11 11:13:14 +0900

    create pagination

--- src/app/Http/Controllers/GrapeorderController.php
+++ src/app/Http/Controllers/GrapeorderController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class GrapeorderController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $products = Product::all();
+        $products = Product::Paginate(2);
+        return view('index', compact('products'));
     }
 }
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -22,50 +22,42 @@
         <div class="order-form__heading">
             <h2>注文票</h2>
         </div>
-        <form class="order-form">
-            <div class="order-form__item">ピオーネ</div>
-            <div class="order-form__quantity">
-                <select class="order-form__quantity-select">
-                    <option value="">房数</option>
-                    <option value="1">1房</option>
-                    <option value="2">2房</option>
-                    <option value="3">3房</option>
-                </select>
+        <div class="order-form">
+        @foreach($products as $product)
+            <div class="order-form__content-group">
+                <div class="order-form__content-image">
+                    <img src="{{ asset('storage/images/' . $product->img_path) }}" alt="{{ $product->name }}" width="200">
+                </div>
+                <div class="order-form__content-name">
+                    <input class="order-form__item-input" type="text" name="name" value="{{ $product['name'] }}">
+                </div>
+                <div class="order-form__content-quantity">
+                    <input class="order-form__quantity-input" type="number" name="quantity" value="{{ $product['quantity'] }}">房
+                </div>
+                <div class="order-form__content-price">
+                    <input class="order-form__quantity-input type="number" name="price" value="{{ number_format($product['price']) }}">円
+                </div>
+                <div class="order-form__number">
+                    数量
+                    <select class="order-form__number-input">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                </div>
+                <div class="order-form__button">
+                    <button class="order-form__button-submit">カートに入れる</button>
+                </div>
             </div>
-            <div class="order-form__price">1000円</div>
-            <div class="order-form__button">
-                <button class="order-form__button-submit" type="submit">注文</button>
-            </div>
-        </form>
-        <form class="order-form">
-            <div class="order-form__item">シャインマスカット</div>
-            <div class="order-form__quantity">
-                <select class="order-form__quantity-select">
-                    <option value="">房数</option>
-                    <option value="1">1房</option>
-                    <option value="2">2房</option>
-                    <option value="3">3房</option>
-                </select>
-            </div>
-            <div class="order-form__price">1000円</div>
-            <div class="order-form__button">
-                <button class="order-form__button-submit" type="submit">注文</button>
-            </div>
-        </form>
-        <div class="order-form__heading">
-            <h2>注文商品一覧</h2>
+
+        @endforeach
         </div>
-        <div class="order-table">
-            <table class="order-table__inner">
-                <tr class="order-table__row">
-                    <tn class="order-table__header">
-                        <span class="order-table__header-span">商品名</span>
-                        <span class="order-table__header-span">注文数</span>
-                        <span class="order-table__header-span">金額</span>
-                    </tn>
-                </tr>
-            </table>
+        <div class="paginate">
+            {{ $products->links() }}
         </div>
-    </div>
+
+
 </body>
 </html>
